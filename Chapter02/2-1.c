e96d6c844a8bdc2de6e5494d55cade4002939f9f
Youngha Park <youngha.kkoti@gmail.com>
2024-09-15 21:03:29 +0900

    2-1

---
+++ Chapter02/2-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+  int A[5] = {10, 20, 30}, B[5];
+  int x = 2018, y;
+
+  y = x;
+  for (int i = 0; i < 5; i++) {
+    B[i] = A[i];
+  }
+
+  printf("Variable copy result: x=%d, y=%d\n", x, y);
+  printf("Array copy result:\n");
+  for (int i = 0; i < 5; i++) {
+    printf("A[%d]=%d\t", i, A[i]);
+    printf("B[%d]=%d\n", i, B[i]);
+  }
+}
