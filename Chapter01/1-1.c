06450fdd4865189434333ea78f3fd2b8bfa13f74
Youngha Park <youngha.kkoti@gmail.com>
2024-09-15 22:19:25 +0900

    1-1

--- Chapter01/1-1.c
+++ Chapter01/1-1.c
@@ -1,6 +1,6 @@
 int find_array_max(int score[], int n) {
-  int i, tmp = score[0];
-  for (i = 1; i < n; i++) {
+  int tmp = score[0];
+  for (int i = 1; i < n; i++) {
     if (score[i] > tmp) {
       tmp = score[i];
     }
